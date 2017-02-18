1c85c5507d9fc91ae65ca581d2062b1ebc095e52
dridk <sacha@labsquare.org>
2017-02-18 16:01:05 +0100

    Annotation model done

--- test1/model.py
+++ test1/model.py
@@ -1,5 +1,10 @@
+import os
 from peewee import * 
 
+try:
+	os.remove("variant.db")
+except : 
+	pass
 db = SqliteDatabase('variant.db')
 class Variant(Model):
 	chrom = CharField()
@@ -9,6 +14,17 @@ class Variant(Model):
 	class Meta:
 		database = db 
 
+class Field(Model):
+	colname = CharField()
+	desc    = CharField()
+	class Meta:
+		database = db
+
+class Annotation(Model):
+	variant = ForeignKeyField(Variant, related_name='variant')
+	class Meta:
+		database = db
+
 
 db.connect()
-db.create_tables([Variant], safe = True)
+db.create_tables([Variant, Field], safe = True)
--- test1/test.py
+++ test1/test.py
@@ -1,15 +1,41 @@
 import sys 
 import vcf 
+import re
 from model import * 
-
+import peewee
 
 filename = sys.argv[1]
 print("import", filename)
 
-reader = vcf.Reader(open(filename,'rb'))
+reader = vcf.Reader(open(filename,'r'))
+
+#  PARSE HEADERS AND CREATE ANNOTATION TABLE ACCORDING FIELDS NUMBER
+for info in reader.infos:
+	if info == "ANN":
+		desc = reader.infos["ANN"].desc
+		bloc = re.findall(r"'(.+)'", desc)
+		fields = [i.strip() for i in bloc[0].split("|")]
+		for i in range(len(fields)):
+			key = info +"_" + str(i+1)
+			f = Field(colname = key, desc = fields[i])
+			f.save()
+			# Append new columns no annotation 
+			new_col = peewee.CharField(null=True)
+			new_col.add_to_class(Annotation, key)
+		# Create annotation table
+		Annotation.create_table()
+
+# IMPORT VARIANT AND ANNOTATION DATA
 with db.transaction():
 	for record in reader:
-		Variant.create(chrom = record.CHROM, pos = record.POS, ref = record.REF, alt = record.ALT[0] )
-	  
-
-
+		variant = Variant.create(chrom = record.CHROM, pos = record.POS, ref = record.REF, alt = record.ALT[0] )
+		if "ANN" in record.INFO:
+			ann = record.INFO["ANN"]
+			m = Annotation()
+			m.variant = variant
+			i = 1
+			for value in ann[0].split("|"):
+				key = "ANN_"+str(i)
+				setattr(m,key,value)
+				i+=1
+			m.save()
